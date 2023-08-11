bebdd23ddceda010d74e13d6970a7e7e7d8cbafb
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-08-11 15:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function threeSum(arr, target) {
-// write your code here
+arr.sort((a,b)=>a-b);
+ let start= 0;
+ let size = arr.length;
+   let closestSum = Number.MAX_SAFE_INTEGER;
+   for(let i =start;i<size-2;i++){
+     let startValue = arr[i];
+     let left = i+1;
+     let right = size-1;
+     while(left<right){
+      let currLeftValue = arr[left];
+      let currRightValue = arr[right];
+      if(currLeftValue+currRightValue<target-arr[i]){
+         closestSum= Math.min(closestSum, Math.abs(currLeftValue+currRightValue+arr[i]));
+         left++;
+      }	
+      else{
+         closestSum= Math.min(closestSum, Math.abs(currLeftValue+currRightValue+arr[i]));
+         right--;
+      }	
+     }
+   }
+	return closestSum;
   
 }
 
